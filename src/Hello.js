f0bbf5bfa926d0f96e0ae3867bb1737aca95024c
Uzma <uzma93raja@gmail.com>
2020-06-13 20:14:53 +0500

    react app

---
+++ src/Hello.js
@@ -0,0 +1,7 @@
+import React from 'react';
+import './Hello.css'
+function Hello({firstName}) {
+    return <p className="myname">  This is para in Hello Tag : {firstName} </p> 
+  }
+  
+  export default Hello;
--- src/index.js
+++ src/index.js
@@ -3,13 +3,10 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
-
 ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
+   <div><App name="Uzma Raja" age={25}/></div>,
   document.getElementById('root')
-);
+ );
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
